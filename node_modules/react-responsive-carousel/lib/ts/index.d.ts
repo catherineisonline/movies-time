10c3bcaa22fff5722ed12d09af751f305ea9522b
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-02-01 20:30:04 +0400

    Delete node_modules directory

--- node_modules/react-responsive-carousel/lib/ts/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Carousel } from './components/Carousel';
-export { CarouselProps } from './components/Carousel/types';
-export { default as Thumbs } from './components/Thumbs';
-//# sourceMappingURL=index.d.ts.map
