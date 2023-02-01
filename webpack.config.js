eba08b6d6e9c995e7afd50785f2c090c560b3f08
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-02-01 13:12:56 -0300

    fixing router issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
-
+// import path from 'path'
+// const __dirname = path.dirname(new URL(import.meta.url).pathname)
 module.exports = {
   devtool: 'inline-cheap-module-source-map',
   entry: './index.js',
@@ -8,7 +9,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index_bundle.js',
-    publicPath: '/',
   },
 
   target: 'web',
@@ -17,10 +17,9 @@ module.exports = {
     static: {
       directory: path.join(__dirname, 'public'),
     },
-    // open: true,
-    // hot: true,
+    open: true,
+    hot: true,
     liveReload: true,
-    historyApiFallback: true,
   },
   resolve: {
     extensions: ['', '.js', '.jsx', '.json', '.css'],
