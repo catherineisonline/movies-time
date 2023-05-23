080d6f3346693949624e84c5e8ad8df55b40ead9
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-05-23 14:00:18 -0300

    added responsive design to the landing

--- src/routes/homepage/AllMovies.js
+++ src/routes/homepage/AllMovies.js
@@ -4,33 +4,33 @@ import { motion } from 'framer-motion'
 
 const AllMovies = ({ movieList, PaginatedItems, getMovie }) => {
   return (
-    <section>
+    <React.Fragment>
       <ul className="homepage-movies">
         {movieList.map((movie) => (
-          <motion.div  whileHover={{ scale: 1.1 }} key={movie.id}>
-          <li  className="homepage-movie">
-            <Link
-              onClick={() => {getMovie(movie.id); }}
-              to={`/movies/${movie.title.toLowerCase().replace(/ /g, '-')}`}
-            >
-              <img
-               alt={`${movie.title}`}
-                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-              />
-            </Link>
-            <section className="homepage-movie-meta">
-              <h3 className="movie-title">{movie.title}</h3>
-              <section className="homepage-movie-meta-title">
-                <p>{movie.release_date.slice(0, 4)}</p>
-                <h3 className="all-movies-rating">{movie.vote_average}</h3>
+          <motion.div whileHover={{ scale: 1.1 }} key={movie.id}>
+            <li className="homepage-movie">
+              <Link
+                onClick={() => { getMovie(movie.id); }}
+                to={`/movies/${movie.title.toLowerCase().replace(/ /g, '-')}`}
+              >
+                <img
+                  alt={`${movie.title}`}
+                  src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                />
+              </Link>
+              <section className="homepage-movie-meta">
+                <h3 className="movie-title">{movie.title}</h3>
+                <section className="homepage-movie-meta-title">
+                  <p>{movie.release_date.slice(0, 4)}</p>
+                  <h3 className="all-movies-rating">{movie.vote_average}</h3>
+                </section>
               </section>
-            </section>
-          </li>
+            </li>
           </motion.div>
         ))}
       </ul>
       {PaginatedItems}
-    </section>
+    </React.Fragment>
   )
 }
 
--- src/routes/homepage/Hero.js
+++ src/routes/homepage/Hero.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
 import { Carousel } from 'react-responsive-carousel'
+import './hero.css'
 
 const Hero = ({ trendingMovies, getMovie }) => {
   return (
