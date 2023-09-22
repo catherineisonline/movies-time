606484eb13c84a8e3d484603d9b0ac1ff3baa8fe
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-09-22 15:44:23 +0400

    Add functionality to close or remove search results when clearing the input or clicking outside the results

--- src/components/HeaderTwo.js
+++ src/components/HeaderTwo.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './header.css'
 import { Link } from 'react-router-dom'
 import DarkWhite from '../assets/images/dark-theme-white.png'
@@ -19,7 +19,19 @@ const HeaderTwo = ({
   theme,
   setThemeMode
 }) => {
+  const ref = useRef();
 
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        setQuery('');
+      }
+    };
+    document.addEventListener('click', handleOutsideClick);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [setQuery]);
 
   return (
     <nav className={`navigation-two `}>
@@ -42,24 +54,32 @@ const HeaderTwo = ({
               value={query}
               onChange={(e) => setQuery(e.target.value)}
             />
-            <button className="search-btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                width="90"
-                height="90"
-                viewBox="0 0 30 30"
+
+            {searchResults.length > 0 ? <button className="search-btn" onClick={() => setQuery("")}>
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50"
                 fill="#e4e4e4"
               >
-                <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
+                <path d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"></path>
               </svg>
-            </button>
+            </button> :
+              <button className="search-btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="90"
+                  height="90"
+                  viewBox="0 0 30 30"
+                  fill="#e4e4e4"
+                >
+                  <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
+                </svg>
+              </button>
+            }
           </section>
 
-
           {searchResults && searchResults.length > 0 ? (
-            <ul className="search-results">
+            <ul className="search-results" ref={ref}>
               {searchResults.map((result, index) => (
                 <li key={index}>
                   <Link onClick={() => {
