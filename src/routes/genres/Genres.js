4b44a896ad17383793098f23b1cf90c7525e72b7
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-09-26 15:25:17 +0400

    Add missing keys to single genre

--- src/routes/genres/Genres.js
+++ src/routes/genres/Genres.js
@@ -23,26 +23,24 @@ const Genres = ({
       <h2>{currentGenre}</h2>
       <ul className="genres-grid">
         {genreListMovies.map((movie) => (
-          <motion.div whileHover={{ scale: 1.1 }}>
-            <li key={movie.id} className="homepage-movie">
-              <Link
-                onClick={() => getMovie(movie.id)}
-                to={`/movies/${movie.title.toLowerCase().replace(/ /g, '-')}`}
-              >
-                <img
-                  alt={`${movie.title}`}
-                  src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                />
-              </Link>
-              <section className="homepage-movie-meta">
-                <h3 className="movie-title">{movie.title}</h3>
-                <section className="homepage-movie-meta-title">
-                  <p>{movie.release_date.slice(0, 4)}</p>
-                  <h3 className="all-movies-rating">{movie.vote_average}</h3>
-                </section>
+          <motion.li key={movie.id} className="homepage-movie" whileHover={{ scale: 1.1 }}>
+            <Link
+              onClick={() => getMovie(movie.id)}
+              to={`/movies/${movie.title.toLowerCase().replace(/ /g, '-')}`}
+            >
+              <img
+                alt={`${movie.title}`}
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+              />
+            </Link>
+            <section className="homepage-movie-meta">
+              <h3 className="movie-title">{movie.title}</h3>
+              <section className="homepage-movie-meta-title">
+                <p>{movie.release_date.slice(0, 4)}</p>
+                <h3 className="all-movies-rating">{movie.vote_average}</h3>
               </section>
-            </li>
-          </motion.div>
+            </section>
+          </motion.li>
         ))}
       </ul>
       {PaginatedItems}
